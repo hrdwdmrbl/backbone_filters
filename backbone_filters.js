c4884cc53498876e8ca626ae5c0025bf69963928
Richard Washer <richard@rwasher.com>
2013-01-30 11:59:58 +1300

    Updated to 0.9.10
    Using https://github.com/jseppi/backbone_filters/commit/2b2e6411678ab2ac8a88d4c3bb48a01e48cae947

--- backbone_filters.js
+++ backbone_filters.js
@@ -25,12 +25,14 @@
 		route: function(route, name, callback) {
 			Backbone.history || (Backbone.history = new Backbone.History);
 			if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+			if (!callback) callback = this[name];
 			Backbone.history.route(route, _.bind(function(fragment) {
 				var args = this._extractParameters(route, fragment);
 				if (this._runFilters(this.before, fragment, args)) {
-					callback.apply(this, args);
+					callback && callback.apply(this, args);
 					this._runFilters(this.after, fragment, args);
 					this.trigger.apply(this, ['route:' + name].concat(args));
+					Backbone.history.trigger('route', this, name, args);
 				}
 			}, this));
 		}
